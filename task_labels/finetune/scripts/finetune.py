28eee1bf894fc761faf40f0499c99e8451caed85
owanr <risakoowan@gmail.com>
2023-10-23 17:01:44 -0500

    a place to save messy code for now

--- task_labels/finetune/scripts/finetune.py
+++ task_labels/finetune/scripts/finetune.py
@@ -1,8 +1,8 @@
-#get_ipython().system('pip install pytesseract datasets rouge-score nltk tensorboard py7zr --upgrade')
-#get_ipython().system('sudo apt-get install git-lfs --yes')
 import evaluate
-import nltk
-#nltk.download("punkt")
+import os
+os.environ["WANDB_PROJECT"] = "artifacts"
+os.environ["WANDB_LOG_MODEL"] = "checkpoint"
+import torch
 import random
 from random import randrange        
 import json
@@ -11,50 +11,149 @@ from datasets import load_dataset, concatenate_datasets, DatasetDict, Dataset
 import numpy as np
 from huggingface_hub import HfFolder
 from transformers import (
-        pipeline,
-        Seq2SeqTrainer, 
-        Seq2SeqTrainingArguments,
-        DataCollatorForSeq2Seq,
-        AutoTokenizer, 
-        AutoModelForSeq2SeqLM)
+    pipeline,
+    )
+import nltk
+from transformers import DataCollatorForSeq2Seq, AutoModelForSeq2SeqLM, DataCollatorWithPadding
 from peft import get_peft_model, LoraConfig, TaskType
 from accelerate import Accelerator
 accelerator = Accelerator()
-#DATA [['model_name', 'dataset_name', 'text_ind', 'text', 'prompt', 'params', 'human_agg', 'model_annots'],
-# intramodel [['dataset_name', 'text_ind', 'text', 'prompt', 'params', 'human_annots', 'model_annots']
-# intermodel************ DATA [['model_name', 'dataset_name', 'text_ind', 'text', 'prompt', 'human_annots', 'model_annots'] 
+import math
 SEED = 42
 random.seed(SEED)
-MODEL_ID = "google/flan-t5-small"    
 import utils
-# code for loading csv into huggingface dataset
+#DATA [['model_name', 'dataset_name', 'text_ind', 'text', 'prompt', 'params', 'human_agg', 'model_annots'],
+# intramodel [['dataset_name', 'text_ind', 'text', 'prompt', 'params', 'human_annots', 'model_annots']
+# intermodel************ DATA [['model_name', 'dataset_name', 'text_ind', 'text', 'prompt', 'human_annots', 'model_annots']
+from custom_trainer import CustomTrainer
+# we want to ignore tokenizer pad token in the loss
+label_pad_token_id = -100
+# Data collator
+BATCH_SIZE = utils.get_batch_size()
 
-data_df = read_csv('../data/data.csv')
-data_df = data_df.fillna(-1)
-
-# create barplot of integers inside human_annots column based on frequency
-data_df['human_agg'].value_counts().plot(kind='bar')
-
-tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
-
-peft_config = LoraConfig(
-    task_type=TaskType.SEQ_2_SEQ_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1
-)
+def calc_num_outcomes(num_labels):
+    # all possible combinations (ignore order) of num_labels)
+    return math.factorial(5)/(math.factorial(5-num_labels)*math.factorial(num_labels))
 
 
-model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_ID)
-model = get_peft_model(model, peft_config)
-model.print_trainable_parameters()
-model.to(accelerator.device)
 
-def compute_metrics(eval_preds):
+def debug_dc():
+    return DataCollatorWithPadding(tokenizer=self.tokenizer)#, pad_to_multiple_of=8)
+
+class Model:
+    def __init__(self, model_name, num_labels=0):
+        self.model_name = model_name
+        if "roberta" in model_name:
+            from transformers import RobertaTokenizerFast, RobertaForSequenceClassification
+            self.tokenizer = RobertaTokenizerFast.from_pretrained(model_name)
+            self.model = RobertaForSequenceClassification.from_pretrained(model_name, num_labels=calc_num_outcomes(num_labels))
+        elif "t5" in model_name:
+            from transformers import DataCollatorForSeq2Seq, AutoModelForSeq2SeqLM, AutoTokenizer
+            from transformers import T5Tokenizer, T5ForConditionalGeneration
+            self.tokenizer = T5Tokenizer.from_pretrained(model_name)
+            peft_config = LoraConfig(
+                task_type=TaskType.CAUSAL_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1
+                #task_type=TaskType.SEQ_2_SEQ_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1
+            )
+            self.model = T5ForConditionalGeneration.from_pretrained(model_name)
+            self.model = get_peft_model(self.model, peft_config)
+            self.model.print_trainable_parameters()
+            self.model.to(accelerator.device)
+        #elif "t5" in model_name:
+        #    from transformers import T5ForConditionalGeneration, AutoTokenizer
+        #    self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+        #    self.model = T5ForConditionalGeneration.from_pretrained(model_name)
+        else:
+            raise ValueError("Model model_name not supported")
+        self.num_labels = num_labels
+
+    def __repr__(self):
+        return self.model_name
+
+    def set_tokenized_dataset(self, tokenized_dataset):
+        self.tokenized_dataset = tokenized_dataset
+
+    def set_training_var(self, repository_id, compute_metrics):
+        is_roberta = "roberta" in self.model_name
+        if is_roberta:
+            from transformers import Trainer, TrainingArguments
+            trainer = CustomTrainer
+            training_args = TrainingArguments
+        elif "t5" in self.model_name:
+            from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
+            trainer = Seq2SeqTrainer
+            training_args = Seq2SeqTrainingArguments
+        print(accelerator.num_processes)
+        # Define training args
+        self.training_args = training_args(
+            output_dir=repository_id,
+            per_device_train_batch_size=BATCH_SIZE,
+            per_device_eval_batch_size=BATCH_SIZE,
+            predict_with_generate=True,
+            fp16=True,############3
+            fp16_full_eval=True,#########
+            dataloader_num_workers=accelerator.num_processes,
+            learning_rate=5e-4,
+            num_train_epochs=50,
+            logging_dir=f"{repository_id}/logs",
+            logging_strategy="steps",
+            logging_steps=500,
+            evaluation_strategy="epoch",
+            save_strategy="epoch",
+            save_total_limit=2,
+            load_best_model_at_end=True,
+            metric_for_best_model="edit_distance",
+            report_to="wandb",
+            push_to_hub=False,
+            hub_strategy="every_save",
+            hub_model_id=repository_id,
+            hub_token=HfFolder.get_token(),
+        )
+        # Create Trainer instance
+        if not is_roberta:
+            data_collator = DataCollatorForSeq2Seq(
+                tokenizer=self.tokenizer,
+                model=self.model,
+                label_pad_token_id=label_pad_token_id,
+                #pad_to_multiple_of=8
+            )
+        else:
+            from transformers import DataCollatorWithPadding
+            #data_collator = DataCollatorWithPadding(tokenizer=self.tokenizer)#, pad_to_multiple_of=8)
+            print('self.tokenized_dataset["train"]', self.tokenized_dataset["train"].shape)
+            data_collator = DataCollatorForSeq2Seq(
+                tokenizer=self.tokenizer,
+                model=self.model,
+                #label_pad_token_id=-100,
+                #pad_to_multiple_of=8
+            )
+            print('self.tokenized_dataset["val"]', self.tokenized_dataset["val"].shape)
+        self.trainer = trainer(
+            model=self.model,
+            args=self.training_args,
+            data_collator=data_collator,
+            train_dataset=self.tokenized_dataset["train"],
+            eval_dataset=self.tokenized_dataset["val"],
+            compute_metrics=compute_metrics,
+        )
+        
+    def tbd(self):
+        # throwing in here for now
+        self.trainer.train()
+        self.trainer.evaluate()
+        self.model.save_pretrained("output_dir") 
+        self.tokenizer.save_pretrained(repository_id)
+        self.trainer.create_model_card()
+        self.trainer.push_to_hub()
+'''
+def compute_metrics_(eval_preds):
     preds, labels = eval_preds
     if isinstance(preds, tuple):
         preds = preds[0]
-    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+    decoded_preds = self.tokenizer.batch_decode(preds, skip_special_tokens=True)
     # Replace -100 in the labels as we can't decode them.
-    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+    #labels = np.where(labels != -100, labels, self.tokenizer.pad_token_id)
+    decoded_labels = self.tokenizer.batch_decode(labels, skip_special_tokens=True)
 
     edit_distances = []
     assert len(decoded_preds) == len(decoded_labels)
@@ -66,77 +165,85 @@ def compute_metrics(eval_preds):
     # Some simple post-processing
     #######################BEFORE AND AFTER ARE THE SAME #########################
     return {'edit_distance': np.mean(edit_distances)}
-
-# we want to ignore tokenizer pad token in the loss
-label_pad_token_id = -100
-# Data collator
-data_collator = DataCollatorForSeq2Seq(
-    tokenizer,
-    model=model,
-    label_pad_token_id=label_pad_token_id,
-    pad_to_multiple_of=8
-)
-
-def main(filename, remove_columns, repository_id, col_for_num_labels):
-    tokenized_dataset = utils.get_tokenized_data(filename, tokenizer, col_for_num_labels, remove_columns=remove_columns)
-    # Define training args
-    training_args = Seq2SeqTrainingArguments(
-        output_dir=repository_id,
-        per_device_train_batch_size=8,
-        per_device_eval_batch_size=8,
-        predict_with_generate=True,
-        fp16=True,
-        fp16_full_eval=True,
-        dataloader_num_workers=accelerator.num_processes,
-        learning_rate=5e-5,
-        num_train_epochs=5,
-        # logging & evaluation strategies
-        logging_dir=f"{repository_id}/logs",
-        logging_strategy="steps",
-        logging_steps=500,
-        evaluation_strategy="epoch",
-        save_strategy="epoch",
-        save_total_limit=2,
-        load_best_model_at_end=True,
-        # metric_for_best_model="overall_f1",
-        # push to hub parameters
-        report_to="wandb",
-        push_to_hub=False,
-        hub_strategy="every_save",
-        hub_model_id=repository_id,
-        hub_token=HfFolder.get_token(),
-    )
-
-    # Create Trainer instance
-    trainer = Seq2SeqTrainer(
-        model=model,
-        args=training_args,
-        data_collator=data_collator,
-        train_dataset=tokenized_dataset["train"],
-        eval_dataset=tokenized_dataset["val"],
-        compute_metrics=compute_metrics,
-    )
-
-    trainer.train()
+'''
 
 
-    trainer.evaluate()
+def main(filename, model_id, dataset_name, remove_columns, col_for_num_labels=[]):
+    #model = Model("google/t5-v1_1-base")
+    #model = Model("google/flan-t5-small")
+    if dataset_name in ['SChem5Labels', 'Sentiment']:
+        num_labels = 5
+    elif dataset_name in ['SBIC']:
+        num_labels = 3
+    elif dataset_name in ['ghc']:
+        num_labels = 2
+    else:
+        raise Exception("dataset_name not supported or not entered")
+    model = Model(model_id, num_labels=pow(num_labels, 5))
+    tokenized_dataset = utils.get_tokenized_data(filename, dataset_name, model.tokenizer, col_for_num_labels, remove_columns=remove_columns)
+    if 'intra' in filename: 
+        repository_id = f"{model_id.replace('/','-')}-intra_model"
+    else:
+        repository_id = f"{model_id.replace('/','-')}-inter_model"
 
-    model.save_pretrained("output_dir") 
-    # Save our tokenizer and create model card
-    tokenizer.save_pretrained(repository_id)
-    trainer.create_model_card()
-    # Push the results to the hub
-    trainer.push_to_hub()
+    def compute_metrics(eval_preds):
+        preds, labels = eval_preds
+        print("==============", preds)
+        preds = torch.from_numpy(preds) 
+        labels = torch.from_numpy(labels)
+        # Replace -100 in the labels as we can't decode them.
+        preds = np.where(preds != -100, preds, model.tokenizer.pad_token_id)
+        labels = np.where(labels != -100, labels, model.tokenizer.pad_token_id)
+        decoded_preds = model.tokenizer.batch_decode(preds, skip_special_tokens=True)
+        print("DECODED PRED", decoded_preds)
+        decoded_labels = model.tokenizer.batch_decode(labels, skip_special_tokens=True)
+        print("DECODED LABELS", decoded_labels)
+
+        edit_distances = []
+        assert len(decoded_preds) == len(decoded_labels)
+        for i in range(len(decoded_preds)):
+            s1 = decoded_preds[i]
+            if s1 == "":
+                edit_distances.append(-10.0)
+                continue
+            s2 = decoded_labels[i]
+            if len(s2.split()) < num_labels:
+                continue
+            # calculate number of overlapping digits
+            # extract digits in s1 that are in s2
+            s1_digits = [int(s) for s in s1.split() if s.isdigit()]
+            s2_digits = [int(s) for s in s2.split() if s.isdigit()]
+
+            diff = sum(abs(s) for s in s1_digits) - sum(abs(s) for s in s2_digits)
+
+            overlap = set(s1_digits).intersection(set(s2_digits))
+
+            edit_distances.append(-nltk.edit_distance(s1, s2)/10 - diff + len(overlap))
+
+
+        # Some simple post-processing
+        #######################BEFORE AND AFTER ARE THE SAME #########################
+        print("edit_distances", edit_distances)
+        return {'edit_distance': np.mean(edit_distances)}
+        #model.compute_metrics(eval_preds, label_pad_token_id=-100)
+
+    model.set_tokenized_dataset(tokenized_dataset)
+    model.set_training_var(repository_id, compute_metrics)
+    model.trainer.train()
+    model.trainer.evaluate()
+    model.model.save_pretrained("output_dir") 
+    model.tokenizer.save_pretrained(repository_id)
+    model.trainer.create_model_card()
+    model.trainer.push_to_hub()
 
 
 # Hugging Face repository id
-repository_id = f"{MODEL_ID.split('/')[1]}-intra_model"
 col_for_num_labels = "human_annots"
 #def main(filename, remove_columns, repository_id, col_for_num_labels):
 main(filename = '../data/intramodel_data.csv', 
+     model_id = "google/t5-v1_1-large",#"roberta-base",
+     dataset_name = "SChem5Labels",
      remove_columns = ['dataset_name', 'text_ind', 'prompt', 'params', 'human_annots'],
-     repository_id = f"{MODEL_ID.split('/')[1]}-intra_model",
      col_for_num_labels = "model_annots")
 '''
 main(filename = '../data/intermodel_data.csv', 
@@ -164,4 +271,8 @@ main(filename = '../data/intermodel_data.csv',
 
 #print(f"flan-t5-base summary:\n{res[0]['summary_text']}")
 
+#data_df = read_csv('../data/data.csv')
+#data_df = data_df.fillna(-1)
+# create barplot of integers inside human_annots column based on frequency
+#data_df['human_agg'].value_counts().plot(kind='bar')
 
--- task_labels/finetune/scripts/utils.py
+++ task_labels/finetune/scripts/utils.py
@@ -1,35 +1,163 @@
 import numpy as np
 from pandas import read_csv
 from datasets import Dataset, DatasetDict
+from torch.utils.data import DataLoader
+from accelerate import Accelerator
+accelerator = Accelerator()
+import nltk 
+import random
+from collections import Counter
+BATCH_SIZE = 16 
+RANDOM_SEED = 42
 
-def format_dataset(filename):
+def get_batch_size():
+    return BATCH_SIZE
+
+def seed_init_fn(x):
+    seed = args.seed + x
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    return
+
+def get_num_labels(dataset_name):
+    if dataset_name in ['SChem5Labels', 'Sentiment']:
+        num_labels = 5
+    elif dataset_name in ['SBIC']:
+        num_labels = 3
+    elif dataset_name in ['ghc']:
+        num_labels = 2
+    else:
+        raise Exception("dataset_name not supported or not entered")
+    return num_labels
+
+def format_dataset(filename, dataset_name, mode="sorted"):
+    np.random.seed(RANDOM_SEED)
     df = read_csv(filename)
+    df = df[df['dataset_name'] == dataset_name]
     # since nans are already removed here (still checking below just in case), we may have lists that are shorter than expected
-    for col in ['human_annots', 'model_annots']:
-        df[col] = df[col].apply(lambda x: sorted([i if i != 'nan' else -1 for i in np.fromstring(x[1:-1].replace('.',''), dtype=int, sep=' ')]))
-        df[f'{col}_str'] = df[col].apply(lambda x: ' '.join([str(i) for i in x]))
-    df['short_prompt'] = df['prompt'].apply(lambda x: x[(x.index("Sentence: ")):])
+    if mode == "sorted":
+        for col in ['human_annots', 'model_annots']:
+            df[col] = df[col].apply(lambda x: sorted([i if i != 'nan' else -1 for i in np.fromstring(x[1:-1].replace('.',''), dtype=int, sep=' ')]))
+            df[f'{col}_str'] = df[col].apply(lambda x: ' '.join([str(i) for i in x]))
+    elif "frequency" in mode:# [frequency, reverse_frequency]
+        for col in ['human_annots', 'model_annots']:
+            for i in range(df[col].shape[0]):
+                this_str = df[col][i][1:-1].replace(" ", "").replace("nan", "").replace(".", "")
+                # adding 1 of each label so it shows up in final string - won't mess up frequency order
+                #this_str += ''.join([str(num) for num in range(get_num_labels(dataset_name))])
+                freq_dict = dict(Counter([row for row in this_str]))
+                freq_dict = dict(sorted(freq_dict.items(), key=lambda x: x[1], reverse=(mode=="frequency")))
+                new_str = ''.join([str(k)*freq_dict[k] for k in freq_dict.keys()])
+                df[col][i] = list(new_str)
+            df[f'{col}_str'] = df[col].apply(lambda x: (' '.join(list(x))))
+    elif mode == "shuffle":
+            df[col] = df[col].apply(lambda x: np.random.shuffle([i if i != 'nan' else -1 for i in np.fromstring(x[1:-1].replace('.',''), dtype=int, sep=' ')]))
+            df[f'{col}_str'] = df[col].apply(lambda x: ' '.join([str(i) for i in x]))
+
+    # remove last sentence fragment for multi-label tasks
+    df['short_prompt'] = df['prompt'].apply(lambda x: x[(x.index("Sentence: ")):].replace("Among the given options, I think the most appropriate option is (",""))
             #.replace("Sentence: ", "##### Sentence #####\n")\
             #.replace("Label options: ", "\n##### Labels options #####\n"))
     return df
 
-def split(df):
+def split(df, suffix=''):
     # count rows in each df
-    train_data = df.sample(frac=0.1, random_state=42)
-    val_data = df.sample(frac=0.1, random_state=42)
-    test_data = df.sample(frac=0.1, random_state=42)
-    #train_data = df.sample(frac=0.8, random_state=42)
-    #val_data = df.drop(train_data.index).sample(frac=0.5, random_state=42)
-    #test_data = df.drop(train_data.index).drop(val_data.index)
+    '''
+    train_data = df.sample(frac=0.01, random_state=42)
+    val_data = train_data
+    #df.sample(frac=0.1, random_state=42)
+    test_data = val_data
+    #df.sample(frac=0.1, random_state=42)
+    '''
+    train_data = df.sample(frac=0.8, random_state=42)
+    val_data = df.drop(train_data.index).sample(frac=0.5, random_state=42)
+    test_data = df.drop(train_data.index).drop(val_data.index)
+    #'''     
+    train_data.reset_index(drop=True, inplace=True)
+    val_data.reset_index(drop=True, inplace=True)
+    test_data.reset_index(drop=True, inplace=True)
+
+    train_data.to_pickle(f"train_data{suffix}.pkl")
+    val_data.to_pickle(f"val_data{suffix}.pkl")
+    test_data.to_pickle(f"test_data{suffix}.pkl")
     return DatasetDict({
         "train": Dataset.from_pandas(train_data),
         "val": Dataset.from_pandas(val_data),
         "test": Dataset.from_pandas(test_data)
     })
 
-def get_data(filename):
-    model_df = format_dataset(filename)
+def get_data(filename, dataset_name):
+    # returns DatasetDict with train, val, test
+    model_df = format_dataset(filename, dataset_name)
     print("=====================+" + filename + "=====================")
     print(model_df)
     dataset = split(model_df) 
+    #print(f"Train dataset size: {len(intra_dataset['train'])}")
+    #print(f"Test dataset size: {len(inter_dataset['test'])}")
     return dataset
+
+def get_dataloader(filename):
+    # untested
+    data = get_data(filename)
+    dataloader = DataLoader(data, batch_size=BATCH_SIZE, shuffle=False, worker_init_fn = seed_init_fn)
+    return dataloader
+
+def get_tokenized_data(filename, dataset, tokenizer, col_for_num_labels, remove_columns):
+    def preprocess_function(sample, target="model_annots_str"):
+    #def preprocess_function(sample, padding="max_length", target="model_annots_str", max_target_length=32):
+        # target is just labels so we can hardcode it as 32
+        # add prefix to the input for t5
+        inputs = []
+        for i in range(len(sample[col_for_num_labels])):
+            #prompt = f"##### Predict a set of annotations from {len(sample[col_for_num_labels][i])} different people #####\n" 
+            #prompt += sample['short_prompt'][i]
+            #prompt += "Answer: The set of annotations from {len(sample[col_for_num_labels][i])} different people is ["
+            prompt = sample['text'][i]
+            inputs.append(prompt)
+        tokenized = tokenizer(inputs, truncation=True)
+        max_source_length = max([len(x) for x in tokenized["input_ids"]])
+        model_inputs = tokenizer(inputs, truncation=True, padding=True)
+        labels = tokenizer(text_target=sample[target], truncation=True, padding=True)
+        #model_inputs = tokenizer(inputs, max_length=max_source_length, padding=padding, truncation=True)
+        #labels = tokenizer(text_target=sample[target], max_length=max_target_length, padding=padding, truncation=True)
+
+        # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
+        # padding in the loss.
+        #if padding == "max_length":
+        #    labels["input_ids"] = [
+        #        [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
+        #    ]
+
+        model_inputs["labels"] = labels["input_ids"]
+        return model_inputs
+
+    data = get_data(filename, dataset)
+    # TODO: maybe pass in columns to remove
+    tokenized_data = data.map(
+            preprocess_function, 
+            batched=True, 
+            batch_size=BATCH_SIZE,
+            num_proc=accelerator.num_processes, remove_columns=remove_columns)
+    return tokenized_data
+
+def compute_metrics(eval_preds, tokenizer, label_pad_token_id=-100):
+    preds, labels = eval_preds
+    if isinstance(preds, tuple):
+        preds = preds[0]
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+    # Replace -100 in the labels as we can't decode them.
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    edit_distances = []
+    assert len(decoded_preds) == len(decoded_labels)
+    for i in range(len(decoded_preds)):
+        s1 = decoded_preds[i]
+        s2 = decoded_labels[i]
+        edit_distances.append(nltk.edit_distance(s1, s2))
+
+    # Some simple post-processing
+    #######################BEFORE AND AFTER ARE THE SAME #########################
+    return {'edit_distance': np.mean(edit_distances)}
+
